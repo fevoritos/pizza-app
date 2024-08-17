e8d5f6612a6c82998b58dc981ed8640cfccb2fbd
fevoritos <fevoritos@mail.ru>
2024-08-17 18:02:18 +0300

    Обработка ошибок и загрузки

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
-import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import { PRFIX } from "../../helpers/API";
 import { Product } from "../../interfaces/product.interface";
 import styles from "./Menu.module.css";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { MenuList } from "./MenuList/MenuList";
 
 export function Menu() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [isLoading, setIsloading] = useState<boolean>(false);
+    const [error, setError] = useState<string | undefined>();
 
     const getMenu = async () => {
         try {
+            setIsloading(true);
+            // await new Promise<void>((resolve) => {
+            //     setTimeout(() => {
+            //         resolve();
+            //     }, 2000);
+            // });
             const { data } = await axios.get<Product[]>(`${PRFIX}/products`);
             setProducts(data);
+            setIsloading(false);
         } catch (e) {
             console.error(e);
+            if (e instanceof AxiosError) {
+                setError(e.message);
+            }
+            setIsloading(false);
             return;
         }
     };
@@ -31,17 +44,9 @@ export function Menu() {
             <Search placeholder="Введите блюдо или состав" />
         </div>
         <div>
-            {products.map(p => {
-                return <ProductCard
-                    id={p.id}
-                    name={p.name}
-                    description={p.ingredients.join(", ")}
-                    rating={p.rating}
-                    price={p.price}
-                    image={p.image}
-
-                />;
-            })}
+            {error && <>{error}</>}
+            {!isLoading && <MenuList products={products} />}
+            {isLoading && <>Загржаем продукты...</>}
         </div>
     </>;
 }
