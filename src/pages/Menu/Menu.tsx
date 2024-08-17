235fb8ae03124256f1076d13c254182f546168b5
fevoritos <fevoritos@mail.ru>
2024-08-17 14:00:32 +0300

    Product Card

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from "../../components/Headling/Headling";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from "./Menu.module.css";
 
@@ -7,6 +8,17 @@ export function Menu() {
         <div className={styles["head"]}>
             <Headling>Меню</Headling>
             <Search placeholder="Введите блюдо или состав" />
+        </div>
+        <div>
+            <ProductCard
+                id={1}
+                title="Наслаждение"
+                description="Салями, руккола, помидоры, оливки"
+                rating={4.5}
+                price={300}
+                image="/product-demo.png"
+
+            />
         </div>
     </>;
 }
