a3ad947c8233b42b00ca3e420980c953ab050f06
fevoritos <fevoritos@mail.ru>
2024-08-20 12:55:01 +0300

    fix key

--- src/pages/Menu/MenuList/MenuList.tsx
+++ src/pages/Menu/MenuList/MenuList.tsx
@@ -3,9 +3,9 @@ import { MenuListProps } from "./MenuList.props";
 import styles from "./MenuList.module.css";
 
 export function MenuList({ products }: MenuListProps) {
-    return <div className={styles.wrapper}>{products.map(p => (
+    return <div className={styles.wrapper}>{products.map((p, i) => (
         <ProductCard
-            key={p.id}
+            key={i}
             id={p.id}
             name={p.name}
             description={p.ingredients.join(", ")}
