30789581672ba9f121e1d2e0ccf10281306bfbf5
fevoritos <fevoritos@mail.ru>
2024-08-23 10:52:29 +0300

    sarch

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -41,9 +41,8 @@ export function Layout() {
                         [styles.active]: isActive
                     })}>
                         <img src="/cart-icon.svg" alt="Иконка корзины" />
-                        Корзина
+                        Корзина <span className={styles["cartCount"]}>{items.reduce((acc, i) => acc += i.count, 0)} </span>
                     </NavLink>
-                    {items.reduce((acc, i) => acc += i.count, 0)}
                 </div>
                 <Button className={styles["exit"]} onClick={logout}>
                     <img src="/exit-icon.svg" alt="Иконка выхода" />
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
 import Search from "../../components/Search/Search";
 import { PREFIX } from "../../helpers/API";
@@ -11,11 +11,22 @@ export function Menu() {
     const [products, setProducts] = useState<IProduct[]>([]);
     const [isLoading, setIsloading] = useState<boolean>(false);
     const [error, setError] = useState<string | undefined>();
+    const [filter, setFilter] = useState<string>();
 
-    const getMenu = async () => {
+    useEffect(() => {
+        getMenu(filter);
+    }, [filter]);
+
+
+
+    const getMenu = async (name?: string) => {
         try {
             setIsloading(true);
-            const { data } = await axios.get<IProduct[]>(`${PREFIX}/products`);
+            const { data } = await axios.get<IProduct[]>(`${PREFIX}/products`, {
+                params: {
+                    name
+                }
+            });
             setProducts(data);
             setIsloading(false);
         } catch (e) {
@@ -28,20 +39,20 @@ export function Menu() {
         }
     };
 
-    useEffect(() => {
-        getMenu();
-    }, []);
-
+    const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+        setFilter(e.target.value);
+    };
 
     return <>
         <div className={styles["head"]}>
             <Headling>Меню</Headling>
-            <Search placeholder="Введите блюдо или состав" />
+            <Search placeholder="Введите блюдо или состав" onChange={updateFilter} />
         </div>
         <div>
             {error && <>{error}</>}
-            {!isLoading && <MenuList products={products.concat(products).concat(products).concat(products)} />}
+            {!isLoading && products.length > 0 && <MenuList products={products} />}
             {isLoading && <>Загржаем продукты...</>}
+            {!isLoading && products.length === 0 && <>Не найдено блюд по запросу</>}
         </div>
     </>;
 }
