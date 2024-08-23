b6b3ed9dc39a938a049d3b6e264c40f686c5f458
fevoritos <fevoritos@mail.ru>
2024-08-23 15:36:40 +0300

    cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,16 +1,39 @@
-// import { useSelector } from "react-redux";
-// import Headling from "../../components/Headling/Headling";
-// import { RootState } from "../../store/store";
-// import CartItem from "../../components/CartItem/CartItem";
-// import { useState } from "react";
-// import { IProduct } from "../../interfaces/product.interface";
+import { useSelector } from "react-redux";
+import Headling from "../../components/Headling/Headling";
+import { RootState } from "../../store/store";
+import CartItem from "../../components/CartItem/CartItem";
+import { useEffect, useState } from "react";
+import { IProduct } from "../../interfaces/product.interface";
+import axios from "axios";
+import { PREFIX } from "../../helpers/API";
 
 export function Cart() {
-    // const [cartProduct, setCartProduct] = useState<IProduct[]>();
-    // const items = useSelector((s: RootState) => s.cart.items);
+    const [cartProducts, setCartProducts] = useState<IProduct[]>([]);
+    const items = useSelector((s: RootState) => s.cart.items);
 
-    // return <>
-    //     <Headling>Корзина</Headling>
-    //     {items.map(i => (<CartItem />))}
-    // </>;
+    const getItem = async (id: number) => {
+        const { data } = await axios.get<IProduct>(`${PREFIX}/products/${id}`);
+        return data;
+    };
+
+    const loadAllItems = async () => {
+        const res = await Promise.all(items.map(i => getItem(i.id)));
+        setCartProducts(res);
+    };
+
+    useEffect(() => {
+        loadAllItems();
+    }, [items]);
+
+    return <>
+        <Headling>Корзина</Headling>
+        {items.map(i => {
+            const product = cartProducts.find(p => p.id === i.id);
+            if (!product) {
+                return;
+            }
+            return <CartItem count={i.count} {...product} />;
+
+        })}
+    </>;
 }
